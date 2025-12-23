4df9de560b7f1c66a118df132fefd64c0a09262b
Yevhen Onoprienko <fromzpcity@gmail.com>
2025-12-23 23:59:35 +0200

    02. usefull-things-429-reveal. UI.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 
+import "lenis/dist/lenis.css";
+import "/src/assets/css/style.css";
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,62 @@
+import { ReactLenis } from "lenis/dist/lenis-react";
+
 export default function Page() {
-    return <h1>Hello, Next.js!</h1>;
+    return (
+        <>
+            <ReactLenis root />
+
+            <nav>
+                <p>Reveal Animation</p>
+                <p>Menu</p>
+            </nav>
+
+            <section className="intro">
+                <div className="section-bg">
+                    <img src="/images/img_1.jpg" alt="" />
+                </div>
+
+                <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, veniam?</h1>
+            </section>
+
+            <section className="about">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia accusantium voluptatibus fugit ipsum!
+                    Quasi iure autem inventore nihil nisi velit iste aspernatur et, cumque exercitationem, molestiae
+                    expedita corrupti veniam deserunt?
+                </p>
+            </section>
+
+            <section className="banner-img">
+                <div className="section-bg">
+                    <img src="/images/img_2.jpg" alt="" />
+                </div>
+            </section>
+
+            <section className="services">
+                <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos ducimus nisi unde.</h1>
+            </section>
+
+            <section className="banner-img">
+                <div className="section-bg">
+                    <img src="/images/img_3.jpg" alt="" />
+                </div>
+            </section>
+
+            <section className="cta">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati illo facere aliquid cupiditate
+                    quidem consequatur ratione officiis repellat officia aspernatur. At architecto repudiandae tempora
+                    quod, magnam cumque dolorem. Architecto, aspernatur.
+                </p>
+            </section>
+
+            <section className="outro">
+                <div className="section-bg">
+                    <img src="/images/img_4.jpg" alt="" />
+                </div>
+
+                <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut odio voluptatem pariatur.</h1>
+            </section>
+        </>
+    );
 }
